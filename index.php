6e684682a0b14da81a1a1b39df9fb7d814d411ef
DimaNovik <dimanovik1989@gmail.com>
2016-11-25 15:00:24 +0200

    start template for site just

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html lang="ru" class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html lang="ru" class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html lang="ru" class="lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="ru">
+<!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+	<title>Головне територіальне управління юстиції в Одеській області</title>
+	<meta name="description" content="Офіційний сайт управління юстиції в Одеській області" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="shortcut icon" href="favicon.png" />
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl-carousel/owl.carousel.css" />
+	<link rel="stylesheet" href="libs/countdown/jquery.countdown.css" />
+	<link rel="stylesheet" href="css/fonts.css" />
+	<link rel="stylesheet" href="css/main.css" />
+	<link rel="stylesheet" href="css/media.css" />
+</head>
+<body>
+	<header class="top_header">
+		<div class="header_topline">
+			<div class="container">
+				<div class="col-md-12">
+					<div class="row">
+						
+						<div class="top_links">
+
+						<nav class="maian_mnu clearfix">
+						
+						<button class="main_mnu_button hidden-md hidden-lg"><i class="fa fa-bars"></i></button>
+						<ul>
+
+						<li><a href="#"><img src='img/gerb_ukr.png'></a></li>
+						
+							<li class="active"><a href="">Про управління</a></li>
+							<li><a href="#news">Сервіси</a></li>
+							<li><a href="">Напрями роботи</a></li>
+							<li><a href="">Графіки прийому</a></li>
+							<li><a href="">Вакансії</a></li>
+							<li><a href="">Контакти</a></li>
+						</ul>
+						
+					</nav>
+							
+						</div>
+						<div class="soc_buttons">
+							
+							<a href="https://www.fb.com/%D0%93%D0%BE%D0%BB%D0%BE%D0%B2%D0%BD%D0%B5-%D1%82%D0%B5%D1%80%D0%B8%D1%82%D0%BE%D1%80%D1%96%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5-%D1%83%D0%BF%D1%80%D0%B0%D0%B2%D0%BB%D1%96%D0%BD%D0%BD%D1%8F-%D1%8E%D1%81%D1%82%D0%B8%D1%86%D1%96%D1%97-%D0%B2-%D0%9E%D0%B4%D0%B5%D1%81%D1%8C%D0%BA%D1%96%D0%B9-%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%96-835756333138290/?fref=ts" target="_blank"><i class="fa fa-facebook-square"></i></a>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			
+				<div class="row">
+					<div class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
+					
+						<p align="center">
+						<img src='img/gerb_logo.jpg' class="gerb_logo"></p>
+						<h2>Головне територіальне управління юстиції
+						<br/><br/>
+						в Одеській області</h2>
+					<div class="alert_danger">
+	                    <p><strong>До уваги користувачів!</strong></p>
+	                    <p>Сайт в процесі розробки. Просимо вибачення за тимчасові незручності.</p>
+	  
+	                    <p><strong>Attention users!</strong></p>
+	                    <p>The site is under construction. Sorry for temporary inconvenience.</p>
+	                    </div>
+					</div>
+						<div class="col-md-8 col-lg-8 col-xs-12 col-sm-12 title">
+						
+						<div class="sider_container">
+						<div class="next_button"><i class="fa fa-angle-right"></i></div>
+						<div class="prev_button"><i class="fa fa-angle-left"></i></div>
+						<div class="carousel">
+						
+							<div class="slide_item"><a class="fancybox" data-fancybox-group="group" href="img/news/2.jpg"><img src="img/news/2.jpg" alt="alt" /></a>
+							<div class="caption_block">
+							<div class="text">22 листопада 2016 року відбулось чергове засідання Одеської обласної міжвідомчої координаційно-методичної ради з правової освіти населення.</div>
+							</div>
+							</div>
+							<div class="slide_item"><a class="fancybox" data-fancybox-group="group" href="img/news/3.jpg"><img src="img/news/3.jpg" alt="alt" /></a>
+							<div class="caption_block">
+							<div class="text"></div>
+							</div>
+							</div>
+							
+						</div>
+					</div>
+					</div>
+						
+				</div>
+				<br/>
+				<br/>
+				<div class="row">
+					<div class="col-md-2 col-lg-2 col-xs-2 col-sm-2">
+					</div>
+					<div class="col-md-8 col-lg-8 col-xs-8 col-sm-8">
+						
+	                </div>
+					<div class="col-md-2 col-lg-2 col-xs-2 col-sm-2">
+					</div>
+                </div>
+					
+					
+					<!-- 
+					<div class="next_button"><i class="fa fa-angle-right"></i></div>
+					<div class="prev_button"><i class="fa fa-angle-left"></i></div>
+					<div class="carousel">
+						<div class="slide_item"><a class="fancybox" data-fancybox-group="group" href="img/mo.jpg"><img src="img/mo.jpg" alt="alt" /></a></div>
+						<div class="slide_item"><a class="fancybox" data-fancybox-group="group" href="img/ds.jpg"><img src="img/ds.jpg" alt="alt" /></a></div>
+						<div class="slide_item"><a class="fancybox" data-fancybox-group="group" href="img/mo.jpg"><img src="img/mo.jpg" alt="alt" /></a></div>
+						<div class="slide_item"><a class="fancybox" data-fancybox-group="group" href="img/ds.jpg"><img src="img/ds.jpg" alt="alt" /></a></div>
+					</div>
+				</div> -->
+				</div>
+			</div>
+		
+	</header>
+<!-- 	 <section class="order">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7">
+					<div class="sell_descr">Самые лучшие приложения для вашего телефона</div>
+				</div>
+				<div class="col-md-5">
+					<div class="sell_right">
+						<a href="#callback" class="button fancybox">Заказать разработку</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section> -->
+	<section class="main_content" id='news'>
+	<hr/>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+					<div class="content-text">
+					<p class="title_news">Новини</p>
+						<div class="blog_item">
+							<div class="row">
+							
+								<div class="col-md-12">
+									<h3><a href="//webdesign-master.ru" target="_blank">Проведено годину спілкування "Перемагай зло добром"</a></h3>
+								</div>
+								<div class="col-md-4">
+									<a href="" target="_blank"><img src="img/news/4.jpg" alt="alt"></a>
+								</div>
+								<div class="col-md-8">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur blanditiis, omnis ipsa sunt aspernatur impedit consequatur quasi, molestiae eos magni, tempora nisi ipsam itaque quas esse aut perspiciatis quia, veniam!</p>
+									<p><i class="fa fa-clock-o" aria-hidden="true"></i> 2016-11-25 14:00</p>
+								</div>
+							</div>
+						</div>
+						<div class="blog_item">
+							<div class="row">
+							
+								<div class="col-md-12">
+									<h3><a href="//webdesign-master.ru" target="_blank">Заголовок статьи</a></h3>
+								</div>
+								<div class="col-md-4">
+									<a href="" target="_blank"><img src="img/news/4.jpg" alt="alt"></a>
+								</div>
+								<div class="col-md-8">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur blanditiis, omnis ipsa sunt aspernatur impedit consequatur quasi, molestiae eos magni, tempora nisi ipsam itaque quas esse aut perspiciatis quia, veniam!</p>
+									<p><i class="fa fa-clock-o" aria-hidden="true"></i> 2016-11-25 14:00</p>
+								</div>
+							</div>
+						</div>
+						<div class="blog_item">
+							<div class="row">
+								<div class="col-md-12">
+									<h3><a href="//webdesign-master.ru" target="_blank">Заголовок статьи</a></h3>
+								</div>
+								<div class="col-md-4">
+									<a href="" target="_blank"><img src="img/news/4.jpg" alt="alt"></a>
+								</div>
+								<div class="col-md-8">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur blanditiis, omnis ipsa sunt aspernatur impedit consequatur quasi, molestiae eos magni, tempora nisi ipsam itaque quas esse aut perspiciatis quia, veniam!</p>
+									<p><i class="fa fa-clock-o" aria-hidden="true"></i> 2016-11-25 14:00</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+				<p class="title_anons">Анонси</p>
+					<aside class="right_aside">
+						<ul>
+							<li><a href="#">25.11.2016 - XII Всеукраїнська правнича школа з судочинства</a></li>
+							<li><a href="#">05.12.2016 - Регіональний план заходів з проведення у області Всеукраїнського тижня права</a></li>
+						</ul>
+						<p align="center"><img src='img/banner1.png'/></p>
+					</aside>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- <div class="hidden">
+		<form id="callback" class="pop_form">
+			<h3>Заказать разработку</h3>
+			<input type="text" name="name" placeholder="Ваше имя..." required />
+			<input type="text" name="phone" placeholder="Ваше телефон..." required />
+			<button class="button" type="submit">Заказать</button>
+		</form>
+	</div> -->
+	<!--[if lt IE 9]>
+	<script src="libs/html5shiv/es5-shim.min.js"></script>
+	<script src="libs/html5shiv/html5shiv.min.js"></script>
+	<script src="libs/html5shiv/html5shiv-printshiv.min.js"></script>
+	<script src="libs/respond/respond.min.js"></script>
+	<![endif]-->
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/jquery-mousewheel/jquery.mousewheel.min.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="libs/waypoints/waypoints-1.6.2.min.js"></script>
+	<script src="libs/scrollto/jquery.scrollTo.min.js"></script>
+	<script src="libs/owl-carousel/owl.carousel.min.js"></script>
+	<script src="libs/countdown/jquery.plugin.js"></script>
+	<script src="libs/countdown/jquery.countdown.min.js"></script>
+	<script src="libs/countdown/jquery.countdown-ru.js"></script>
+	<script src="libs/landing-nav/navigation.js"></script>
+	<script src="js/common.js"></script>
+	<!-- Yandex.Metrika counter --><!-- Yandex.Metrika counter --><script type="text/javascript">(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter25346996 = new Ya.Metrika({id:25346996, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="//mc.yandex.ru/watch/25346996" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter --><!-- /Yandex.Metrika counter -->
+	<!-- Google Analytics counter --><!-- /Google Analytics counter -->
+</body>
+</html>
